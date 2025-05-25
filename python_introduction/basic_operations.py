bd3658ec847bb9df3707a3d4bb8db3250aa5d172
Phionanamugga <phionanamugga15@gmail.com>
2025-05-25 12:36:14 +0200

    carries out arithematic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+# Addition
+c = a + b
+# Subtraction
+d = a - b
+# Multiplication
+e = a * b
+
+print("Addition:", c)
+print("Subtraction:", d)
+print("Multiplication:", e)
+
+# Print the results
+print(f"Addition of {a} and {b} is {c}")
+print(f"Subtraction of {a} and {b} is {d}")
+print(f"Multiplication of {a} and {b} is {e}")
